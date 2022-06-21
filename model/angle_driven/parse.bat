0ddad4b7eee28d08c5d23f50b667684fb7c77d83
tom <tomrottier@hotmail.com>
2022-06-21 13:26:04 +0200

    add new arms model

--- model/angle_driven/parse.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-set a=%1.2.json
-:: parse to json
-C:\Users\rottier\Downloads\dotnet-sdk-5.0.408-win-x64\dotnet C:\Users\rottier\Downloads\dotnet-sdk-5.0.408-win-x64\fortran-parser.dll %1
-
-:: parse to julia
-julia --project="C:\Users\rottier\fortran parser" "C:\Users\rottier\fortran parser\parse.jl" %a%
-
